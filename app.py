d351ee48f0e94e52ba5f9651966d80bc2c2c27a2
catru2 <camachotrujillomartinx@gmail.com>
2024-11-27 11:16:48 -0600

    cambio

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ grammar = """
 ?term: factor
      | term "*" factor -> mul
      | term "/" factor -> div
-?factor: NUMBER        -> number
+?factor: SIGNED_NUMBER -> number
+       | "-" factor    -> neg
        | "(" expr ")"  -> parens
-NUMBER: /\d+(\.\d+)?/
-%ignore " "            // Ignorar espacios en blanco
+SIGNED_NUMBER: /-?\d+(\.\d+)?/
+%ignore " "                     // Ignorar espacios en blanco
 """
+
+
 
 parser = Lark(grammar, start='start', parser='lalr')
 
